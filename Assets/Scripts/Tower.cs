9bd963ddc370f0bc49574381cfa3e3fd7c3ae961
t1az2z <tyukovia@gmail.com>
2018-05-08 16:14:09 +0300

    Cannons added. LookAtEnemy script for cannons added

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+	
+
+    void LookAtEnemy(Transform cannon, Transform enemy)
+    {
+        cannon.LookAt(enemy, Vector3.up);
+    }
+	void Update () {
+
+        LookAtEnemy(objectToPan, targetEnemy);
+
+	}
+}
